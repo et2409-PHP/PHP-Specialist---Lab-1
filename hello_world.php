4ece4d1eff02e0d51c01f7f282105ffac7af882b
Eyvette Thompson <eyvettet@gmail.com>
2017-10-05 00:50:44 -0400

    Add files via upload

---
+++ hello_world.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Integrated PHP</title>
+</head>
+<body>
+
+<?php
+print ("<h1>Hello World</h1>");
+?>
+<p>Welcome to PHP Specialist</p>
+<p>Today is <?php print(date("1")); 
+?></p> 
+    
+</body>
+</html>
---
+++ restaurants.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Restaurants</title>
+</head>
+<body>
+<?php
+    
+print("<h1>My Favorite Restaurants</h1>");
+    
+print("<ul>"); 
+
+print("<li>Shuckers</li>");
+print("<li>Sonic</li>");    
+print("<li>Shanes</li>");    
+print("<li>Waffle House</li>");    
+print("<li>New Japan</li>"); 
+
+    
+print("</ul>");    
+    
+    
+?>    
+    
+    
+</body>
+
+</html>
